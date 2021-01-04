fd32dd4b6fd6e4052a3147c1d3caecf1e0bf4910
Gabriel <gabrielkollun@gmail.com>
2021-01-04 12:02:19 -0300

    First MVC - Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+﻿using SalesWebMvc.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
